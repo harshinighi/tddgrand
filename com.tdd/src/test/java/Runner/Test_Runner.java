4471d62168c503ddc231ad20dd4e00976ff21b4f
IBM <IBM@KAMAL-REDDY>
2018-08-29 16:36:25 +0530

    sent

---
+++ com.tdd/src/test/java/Runner/Test_Runner.java
@@ -0,0 +1,31 @@
+package Runner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test_Runner {
+WebDriver driver;
+@Test
+public void test() 
+{
+	System.out.println(driver.getTitle());
+	//System.out.println("hmmmmmmm");
+	}
+@BeforeTest
+public void beforeTest()
+{
+	System.setProperty("webdriver.gecko.driver", "/gecko/geckodriver");
+	//System.setProperty("webdriver.gecko.driver", "E:\\SHARED\\geckodriver\\geckodriver.exe");
+	System.out.println("helllooo");
+	driver= new FirefoxDriver();
+	driver.get("http://13.251.143.66:8090/IPL-Fantasy-League/");
+
+}
+@AfterTest
+public void afterTest(){
+	System.out.println("Running1");
+}
+}
